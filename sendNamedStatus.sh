059ffcc8c5b2a41d37712c127214d0ba1dcd7f24
3mdf1v3 <3mdf1v3@gmail.com>
2018-11-18 13:10:22 +0100

    Create sendNamedStatus.sh

---
+++ sendNamedStatus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SUBJECT="Named status"
+FROM="$(VMNAME)@$(HOSTNAME)"
+MAILTO=$(MAILTO)
+
+grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
+
+MESSAGE=$(cat /tmp/sendNamedStatus.txt)
+
+(
+echo "From: ${FROM}";
+echo "To: ${MAILTO}";
+echo "Subject: ${SUBJECT}";
+echo "";
+echo "${MESSAGE}"
+) | sendmail -t
+
+rm -f /tmp/sendNamedStatus.txt
