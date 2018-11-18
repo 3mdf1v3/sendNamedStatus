93f69a2d83406bfcb8f1f08f1cac82cc8783aae8
root <root@ns2.it.dns.opennic.glue>
2018-11-18 22:09:56 +0100

    Added new destination email

--- sendNamedStatus.sh
+++ sendNamedStatus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SUBJECT="Named status"
+MAILTO=$(cat cat /root/.forward)
 
 grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
 
