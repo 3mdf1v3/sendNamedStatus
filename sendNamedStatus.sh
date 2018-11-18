d3433dfa9f82aab3336936c63c5674c163d33863
root <root@ns2.it.dns.opennic.glue>
2018-11-18 22:21:48 +0100

    Bug fix

--- sendNamedStatus.sh
+++ sendNamedStatus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SUBJECT="Named status"
-MAILTO=$(cat /root/.forward)
 
 grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
 
