e075aaa9a0e9c7be6f77b1e2c131cfe912a0fb52
root <root@ns2.it.dns.opennic.glue>
2018-11-18 13:15:35 +0100

    Added VAriable to env

--- sendNamedStatus.sh
+++ sendNamedStatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBJECT="Named status"
-FROM="$(VMNAME)@$(HOSTNAME)"
-MAILTO=$(MAILTO)
+FROM="$(echo VMNAME)@$(echo HOSTNAME)"
+MAILTO=$(echo $MAILTO)
 
 grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
 
