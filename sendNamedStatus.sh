feed5f07c6d9300b65823701f24417f0eb89f5d5
root <root@ns2.it.dns.opennic.glue>
2018-11-18 22:06:48 +0100

    Updated from/to email address

--- sendNamedStatus.sh
+++ sendNamedStatus.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 SUBJECT="Named status"
-FROM="${USERNAME}@${HOSTNAME}"
 
 grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
 
 MESSAGE=$(cat /tmp/sendNamedStatus.txt)
 
 (
-echo "From: ${FROM}";
-echo "To: ${USERNAME}";
+echo "From: root";
+echo "To: root";
 echo "Subject: ${SUBJECT}";
 echo "";
 echo "${MESSAGE}"
