238027a08066848ecb37bf962af0cb00444a6689
root <root@ns2.it.dns.opennic.glue>
2018-11-25 11:40:11 +0100

    added auto ban ip with ip information

--- sendNamedStatus.sh
+++ sendNamedStatus.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-SUBJECT="Named status"
+SUBJECT="Named banned ip"
+SOURCEFILE="/var/log/named/misc.log"
+TMPFILE="/tmp/sendNamedStatus.txt"
+CONFFILE="/etc/named.conf"
+THRESHOLD=1000
 
-grep 'client' /var/log/named/misc.log | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> /tmp/sendNamedStatus.txt
+grep 'client' ${SOURCEFILE} | awk -F' ' '{print $5}' | awk -F'#' '{print $1}' | sort | uniq -c | sort -nr | head -20 >> ${TMPFILE}
 
-MESSAGE=$(cat /tmp/sendNamedStatus.txt)
+while read ROW; do
+	if [ $(echo "${ROW}" | awk -F' ' '{print $1}') -gt ${THRESHOLD} ]; then
+		IP=$(echo "${ROW}" | awk -F' ' '{print $2}')
+		if [ $(grep -c "${IP}" ${CONFFILE}) -eq 0 ]; then
+			INFOIP=$(curl -s 'ipinfo.io/'${IP} | jq -c [.country,.org])
+			sed -i "/acl \"blackhats\"/a\ \ \ \ ${IP}; # ${INFOIP}" "${CONFFILE}"
+			MESSAGE+=${ROW}' '${INFOIP}'\r\n' 
+		fi
+	fi
+done < ${TMPFILE}
 
-(
-echo "From: root";
-echo "To: root";
-echo "Subject: ${SUBJECT}";
-echo "";
-echo "${MESSAGE}"
-) | sendmail -t
+systemctl reload named
 
-rm -f /tmp/sendNamedStatus.txt
+if [ ! -z "${MESSAGE}" ]; then
+	(
+		echo "From: root";
+		echo "To: root";
+		echo "Subject: ${SUBJECT}";
+		echo "";
+		echo -e "${MESSAGE}"
+	) | sendmail -t
+fi
+
+rm -f ${TMPFILE}
